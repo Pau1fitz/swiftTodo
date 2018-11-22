a1175ac5447fb277fbe90c0c645d6ca6115604be
Paul Fitzgerald <paulfitz99@gmail.com>
2018-11-22 12:06:04 +0000

    remove unused function

--- todo/ViewController.swift
+++ todo/ViewController.swift
@@ -41,14 +41,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
